2403b791fcb6f355fca04faf3c7deac6941597ad
Kushal <vankadara2004@gmail.com>
2024-07-15 23:20:05 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Hero from "./pages/app/hero/Hero";
+import Nav from "./components/nav/Nav";
+import Courses from "./pages/app/courses/Courses";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Details from "./pages/app/details/Details";
+import Learn from "./pages/app/learn/Learn";
+import Chapter from "./pages/app/chapter/Chapter";
+import Page404 from "./pages/misc/Page404/Page404";
+import Feedback from "./pages/app/feedback/FeedBack";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(()=> {
+    document.title = "Easy-Learn";
+  }, []);
+
+  const router=createBrowserRouter([
+    {path: "/", element: <Nav />, errorElement: <Page404 />, children: [
+      { index: true, element: <Hero /> },
+      { path: "courses", children: [
+        { index: true, element: <Courses /> },
+        { path: ":courseId", element: <Details /> },
+      ]},
+      { path: "/learn/:courseId", element: <Learn />, children: [
+        { path: "chapter/:chapterId", element: <Chapter />}
+      ]},
+      { path: "feedback", element: <Feedback /> }
+    ]}
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router}></RouterProvider>
   );
 }
 
